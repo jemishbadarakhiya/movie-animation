50ad87fb8ad73e374eee78d23352cf7db486c0a7
Esha <esha.vachhani@spoke.design>
2025-03-04 17:02:09 +0530

    create conflict

--- animation.js
+++ animation.js
@@ -8,14 +8,11 @@ products.forEach(function(item){
         console.log('sample test')
         console.log('merge a testing')
     }
-
 })
-
 
 const products = [
     { id: 1, name: "Laptop", price: 800, category: "Electronics" },
     { id: 2, name: "Phone", price: 500, category: "Electronics" },
-    { id: 7},
     { id: 3, name: "Shoes", price: 100, category: "Fashion" },
     { id: 4, name: "Watch", price: 200, category: "Fashion" },
     { id: 5, name: "Headphones", price: 150, category: "Electronics" }
